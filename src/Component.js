63db8dd72904fe4816ca8815828f50cc558569be
Shubham jain <shubhamjain@192.168.1.4>
2022-11-03 15:32:14 +0530

    handle events with state

--- src/Component.js
+++ src/Component.js
@@ -2,21 +2,51 @@ import React, {useState} from "react";
 
 function Component({name, email, phone}) {
 
-  const [inputText, setInputText] = useState("");
-
+  const [formData, setFormData] = useState({name: "", email: "", phone: ""});
+  const [userList, setUserList] = useState([]);
+  
   const inputHandleChange = (e) => {
-    setInputText(e.target.value)
+    setFormData(prevState => ({...prevState, [e.target.name]: e.target.value}));
   }
+  const handleSubmitClick = () => {
+    setUserList([...userList, formData]);
+    setFormData({name: "", email: "", phone: ""});
+  }
+
+  console.log("userList", userList);
 
 
   return (
     <div>
-      <p>Name - {name}</p>
-      <p>Email - {email}</p>
-      <p>Phone - {phone}</p>
-
-      <input onChange={inputHandleChange}  /> 
-      <p>{inputText}</p>
+      <div>
+        <br/> 
+        <input onChange={(e)=>inputHandleChange(e)} name="name" value={formData.name} placeholder="Please enter your name" />
+        <br/> <br/> 
+        <input onChange={inputHandleChange} name="email" value={formData.email} placeholder="Please enter your email" /> 
+        <br/> <br/> 
+        <input onChange={inputHandleChange} name="phone" value={formData.phone} placeholder="Please enter your phone" /> 
+        <br/> <br/> 
+        <button onClick={handleSubmitClick}>Submit</button>
+        <div>
+          <table style={{border: "1px solid black"}}>
+            <tr>
+              <th style={{border: "1px solid black"}}>Name</th>
+              <th style={{border: "1px solid black"}}>Email</th>
+              <th style={{border: "1px solid black"}}>Phone</th>
+            </tr>
+            {
+              userList.map((user, index)=> (
+                <tr key={index}>
+                  <td style={{border: "1px solid black"}}>{user.name}</td>
+                  <td style={{border: "1px solid black"}}>{user.email}</td>
+                  <td style={{border: "1px solid black"}}>{user.phone}</td>
+                </tr>
+              ))
+            }
+            
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
